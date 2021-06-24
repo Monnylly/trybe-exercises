960f1dd111e828c04c61fb54fab93922d6628b7a
Monnylly <monnylly@gmail.com>
2021-06-24 11:11:57 -0300

    resolvendo exercicios 1,2,3,4,5,8,9,10

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -25,3 +25,20 @@ switch (processoSeletivo) {
   default:
       console.log("valor não identificado")
 }
+
+
+let custo = 15;
+let venda = 25;
+let custoReal = custo + custo*(20/100);
+
+let lucro = (venda - custoReal)* 1000;
+  console.log("o lucro real da empresa será de" + lucro);
+
+
+let salario = 3000;
+let descInss = (salario *11/100);
+let salarioBase = salario - descontolIr;
+let valorIR = (salario * 7.5/100) - 142.80;
+let deducaoIr = salarioBase - valorIR;
+let salarioLiquido = salarioBase - deducaoIR;
+  console.log(salarioLiquido);
