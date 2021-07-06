e4706a26ee70594e8194b83a2140979bc1ca001e
Monnylly <monnylly@gmail.com>
2021-07-06 15:24:42 -0300

    atividades bloco 5 dia 02 elementos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -2,30 +2,42 @@
 //1 Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos.
 
 function mudaTextoTag (){
-  document.getElementsById("paragrafo").innerText = "Desenvolvedora de sucesso";
+  document.getElementById("paragrafo").innerText = "Desenvolvedora de sucesso";
 }
+mudaTextoTag();
 
 //2 Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 function mudaCorDoQuadradoAmarelo (){
   document.getElementsByClassName("main-content")[0].style.background = "rgb(76,164,109";
 }
+mudaCorDoQuadradoAmarelo();
+
 //3Crie uma função que mude a cor do quadrado vermelho para branco.
 function mudaCorQuadradoVermelho(){
-  Document.getElementsByClassName(".main-content .center-content").style.background = "white";
+  document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
 }
+mudaCorQuadradoVermelho();
+
 //4 Crie uma função que corrija o texto da tag <h1>.
 
-function corigeTextH1(){
+function corrigeTextH1(){
   document.getElementsByClassName("title")[0].innerText ="Exercício 5.1 - JavaScript";
 }
+corrigeTextH1();
 
 //5 Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 function mudaTextoParaMaiusculo (){
-  document.getElementsByClassName("center-content") = toUppercase;
+  for (let index = 0; index < document.getElementsByTagName("p").length; index += 1){
+    let resultado = document.getElementsByTagName("p")[index];
+    let resMaiusculo = document.getElementsByTagName("p")[index].innerText.toUpperCase();
+    resultado.innerText = resMaiusculo;
+  }
 }
+mudaTextoParaMaiusculo();
 
 
 //6 Crie uma função que exiba o conteúdo de todas as tags <p> no console
 function exibeConteudo(){
-  console.log(document.getElementsByClassName("center-content"));
+  console.log(document.getElementsByTagName("p")[0]);
 }
+exibeConteudo();
