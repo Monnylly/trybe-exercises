6fb523a14f493c192c2c89b38cfcc3f64941ae1a
Monnylly <monnylly@gmail.com>
2021-07-26 14:14:37 -0300

    resposta parte 2 exercicios

--- bloco7-dia1/script.js
+++ bloco7-dia1/script.js
@@ -49,3 +49,72 @@ const sortedArray = sortOrdersAndEvents();
 console.log(`Os numeros ${sortedArray} se encontram ordenados de forma crescente`);
 
 //Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+
+//parte2
+//Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , 
+//const , arrow functions , template literals e ternary operator 
+//1Crie uma função que receba um número e retorne seu fatorial.
+//Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+//Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+//Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const factorial = number => {
+  let result = 1;
+
+  for (let index = 1; index <= number; index += 1){
+    result *= index;
+  }
+  return result
+}
+console.log(factorial(3));
+
+//Crie uma função que receba uma frase e retorne qual a maior palavra.
+const longstWord = text =>{
+  let wordArray = text.split(' ')
+  let maxLength = 0
+  let result = ''
+
+  for (const word of wordArray){
+    if(word.length > maxLength){
+      maxLength = word.length
+      result = word
+    }
+  }
+  return result
+}
+console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'))
+
+//parte2
+//1 Crie uma página que contenha:
+
+//ex 4
+///resposta da 4
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    );
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`);
+
+    result = `
+{result}
+
+    #goTrybe
+    `;
+
+    return result;
+}
+
+console.log(buildSkillsPhrase("Lucas"));
