c127005d08f218d6262fba996dd7e1ad2ef48e34
Monnylly <monnylly@gmail.com>
2021-08-31 10:00:04 -0300

    organizando pastas

---
+++ modulo-fundamentos/dia-1-primeiros-passos-no-jest/01-sum/02-roman.numerals/roman-numerals.test.js
@@ -0,0 +1,9 @@
+// const { test, expect } = require('@jest/globals');
+// const convertToRoman = require('./roman-numerals');
+
+// test('if conversion of numbers from 1 to 5 are successful', () => {
+//   expect(convertToRoman(2)).toBe('II');
+//   expect(convertToRoman(3)).toBe('III');
+//   expect(convertToRoman(4)).toBe('IV');
+//   expect(convertToRoman(5)).toBe('V');
+// });
